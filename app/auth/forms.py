12092fc78f209f692c22b154ac2cc5e280248042
Adam Henry <adam.henry@gmail.com>
2022-12-06 15:49:27 -0600

    add registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp
+from ..models import User
 
 class LoginForm(FlaskForm):
     user_name = StringField('Username', validators=[DataRequired(), Length(1,64)])
@@ -8,3 +9,14 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
     
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(1,64), 
+        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames can only have letters, numbers, dots or underscores')])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use')
+            
